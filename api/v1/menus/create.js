615e01106a121bdf1a3e7ea9530feff49ade0b9e
Gabriel R. Sezefredo <gabriel@sezefredo.com.br>
2018-04-08 14:56:02 -0300

    Implements menus upload
    This commit implements the menus upload route at menu.post('/').
    This route will receive a file and 3 fields (restaurant_name, restaurant_address, restaurant_city), some checks were implemented to ensure these were sent correctly.
    If everything is received, it will:
     - Upload the file to box (using our File System Client);
     - Delete the temp file from our local file system, so we don't end up with a bunch of unused files in there.
     - And finally, save the entry in our Cloudant (using our DB Client).
    The final DB entry will be returned to the caller.
    
    Signed-off-by: Gabriel R. Sezefredo <gabriel@sezefredo.com.br>

--- api/v1/menus/create.js
+++ api/v1/menus/create.js
@@ -1,4 +1,52 @@
-module.exports = (req, res) => {
-    // TODO: create menu method
-    return res.send('TODO: create menu method.');
+const FSClient = require('../../lib/FSClient');
+const DBClient = require('../../lib/DBClient');
+const fs = require('fs');
+
+const httpStatus = require('../../lib/http-status-codes');
+
+module.exports = (req, res, next) => {
+    let menuEntry = {
+        restaurant: {
+            name: req.body.restaurant_name,
+            address: req.body.restaurant_address,
+            city: req.body.restaurant_city
+        },
+        file: null,
+        ocr: []
+    }
+    // Check if request is missing parameters
+    for (let k in menuEntry.restaurant){
+        if (typeof menuEntry.restaurant[k] === 'undefined' || menuEntry.restaurant[k] === ''){
+            let err = new Error(`Missing field "restaurant_${k}"`);
+            err.status = httpStatus.BAD_REQUEST;
+            throw err;
+        }
+    }
+
+    // Uploading to box and saving to db
+    let fsClient = new FSClient();
+    let dbClient = new DBClient();
+    fsClient.uploadMenu(req.file)
+        .then(id => {
+            menuEntry.file = `/files/${id}`
+            return deleteFile(req.file.path);
+        })
+        .then(() => dbClient.insert(menuEntry, 'menus'))
+        .then(dbEntry => res.status(httpStatus.OK_CREATED).send(Object.assign(dbEntry, menuEntry)))
+        .catch((err) => {
+            return next(err) // Handled by the "menus" specific error handler, in menus/index.js
+        });
 };
+
+// "Promisifying" the fs.unlink function
+function deleteFile(path) {
+    return new Promise((res, rej) => {
+        fs.unlink(path, err => {
+            if (err) {
+                console.log(err.stack);
+                return rej(new Error('Error while trying to delete the temporary file'));
+            }
+            return res();
+        })
+    });
+}
