1390636ce3bed89903757e4c3bea3067636d1984
Gabriel R. Sezefredo <gabriel@sezefredo.com.br>
2018-04-08 14:49:04 -0300

    Import required configurations to the "menus" resource router
    In this commit I use the previously created MulterConfig module in order to create the upload configurator for our menus.post('/') route.
    An edit was necessary in the menus error handler. As noted in the code, file size errors are generated directly by multer/busboy, and because of that it was a bit hard to define a specific error status for this case, so, in order to accomplish this, I had to add an error code check to the "menus" error handler to manually set the error status I wanted when a file too large is being sent. We may be able to improve this later, but I didn't find this to be a blocker.
    
    Signed-off-by: Gabriel R. Sezefredo <gabriel@sezefredo.com.br>

--- api/v1/menus/index.js
+++ api/v1/menus/index.js
@@ -1,3 +1,7 @@
+// File upload configurations
+const MulterConfig = require('../../lib/MulterConfig');
+const upload = new MulterConfig('menus');
+
 const httpStatus = require('../../lib/http-status-codes');
 
 const menus = require('express').Router();
@@ -16,12 +20,22 @@ menus.get('/', (req, res) => {
     return getAll(req, res);
 });
 menus.get('/:id', getOne);
-menus.post('/', create);
+menus.post('/', upload.single('file'), create);
 menus.delete('/:id', del);
 menus.use('/:id/ocr', ocr);
 
 // Error Handler for menus
 menus.use(function (err, req, res, next) {
+    /*
+        This is not ideal, it's replacing the busboy error when a file is too large.
+        I couldn't find another way to send the error I wanted when this happened.
+        We should think about improving it later on.
+    */
+    if (err.code === 'LIMIT_FILE_SIZE') {
+        err.message += `, maximum file size: ${upload.limits.fileSize/1024/1024}MB`;
+        err.status = httpStatus.BAD_REQUEST;
+    }
+
     console.log(err.stack);
     res.status(err.status || httpStatus.INTERNAL_SERVER_ERROR).send(err.message);
 });
