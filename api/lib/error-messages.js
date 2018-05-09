b79518ca3ea36625bcb8fa7fa81e5401102215c3
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2018-05-09 10:36:58 -0300

    Update error messages to schemas objects
    Signed-off-by: Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>

--- api/lib/error-messages.js
+++ api/lib/error-messages.js
@@ -2,7 +2,7 @@ module.exports.TYPE_MISMATCH_ERROR = function(parameter, expected, received) {
     return `Wrong type for parameter ${parameter}. Received ${received}, expected ${expected}`;
 }
 
-module.exports.MISSING_FIELD_ERROR = function(field){
+module.exports.MISSING_FIELD_ERROR = function(field) {
     return `Missing field "${field}"`;
 }
 
@@ -10,11 +10,22 @@ module.exports.IO_ERROR = function(file){
     return `File read/write error when trying to access file ${file}`;
 }
 
-module.exports.DB_CONNECT_ERROR = () => `Error connecting to the database, check logs for details.`;
+module.exports.DB_CONNECT_ERROR = () => 'Error connecting to the database, check logs for details.';
+
 module.exports.NO_DB_ERROR = () => 'This client has no default databaset set, and one was not provided.';
 
-module.exports.LIMIT_FILE_SIZE_ERROR = function(maximumSize){
+module.exports.LIMIT_FILE_SIZE_ERROR = function(maximumSize) {
     return `The uploaded file exceeds the maximum file size supported of ${maximumSize/1024/1024}MB.`;
 }
 
+module.exports.INVALID_SCHEMA_ERROR = function(validSchemas) {
+    return `Schema must be one of those: ${validSchemas}`;
+}
+
+module.exports.INVALID_SCHEMA_FIELDS_ERROR = function(schemaName, schemaStructure) {
+    return `${schemaName} schema must be like that: \n ${schemaStructure}`;
+}
+
+module.exports.INTERNAL_SERVER_ERROR = () => 'The server encountered an error and could not complete ' +
+                                              'your request. Please try again later.';
 
