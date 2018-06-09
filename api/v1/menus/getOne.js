9173e4c5f8c157c5923c8d41d55f4c23438699c0
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2018-06-09 15:20:51 -0300

    Add getOne menu route
    This route can be used by a get request to /api/v1/menus/:id
    It uses getById dependency from the DBClient to find the
    requested id in it's parameter. If the DBClient returns a
    'missing' error.message the status code to the client is 404,
    otherwise is going to be 500.
    There is no need to specifie the typeof the id parameter in the route
    and it's maximum size or invalid characters, beacause it is already
    defined as a string in the route - and cannot be undefined - and
    the Cloudant db does not have limitations about speacial character
    or limit size to the id key value.
    
    Signed-off-by: Gustavo Porto Guedes gustavo.guedes@fatec.sp.gov.br

--- api/v1/menus/getOne.js
+++ api/v1/menus/getOne.js
@@ -1,4 +1,19 @@
-module.exports = (req, res) => {
-    // TODO: getOne menu method.
-    return res.send('TODO: getOne menu method.');
+const DBClient = require('../../lib/DBClient');
+const httpStatus = require('../../lib/http-status-codes');
+const errorMessages = require('../../lib/error-messages');
+
+const dbInstance = new DBClient('menus');
+
+module.exports = (req, res, next) => {
+    dbInstance.getById(req.params.id)
+        .then((resp) => res.status(httpStatus.OK).json(resp))
+        .catch((err) => {
+            if (err.message === 'missing') {
+                err = new Error(errorMessages.DB_DOC_NOT_FOUND(req.params.id));
+                err.status = httpStatus.NOT_FOUND;
+            } else {
+                err = new Error(errorMessages.INTERNAL_SERVER_ERROR());
+            }
+            return next(err)
+        })
 };
