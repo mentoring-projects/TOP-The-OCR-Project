93c00a413537bdf8def8eb4e7d3a7e144ca039c1
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2018-06-09 15:20:51 -0300

    Add getOne menu route
    This route can be used by a get request to /api/v1/menus/:id
    It uses getById dependency from the DBClient to find the
    requested id in it's parameter. If the DBClient returns a
    'missing' error.message the status code to the client is 404,
    otherwise is going to be 500.
    There is no need to specifie the typeof the id parameter in the route
    and it's maximum size or invalid characters, beacause it is already
    defined as a string in the route - and cannot be undefined - and
    the Cloudant db does not have limitations about speacial character
    or limit size to the id key value.
    
    Signed-off-by: Gustavo Porto Guedes gustavo.guedes@fatec.sp.gov.br

--- api/lib/error-messages.js
+++ api/lib/error-messages.js
@@ -14,6 +14,10 @@ module.exports.DB_CONNECT_ERROR = () => 'Error connecting to the database, check
 
 module.exports.NO_DB_ERROR = () => 'This client has no default databaset set, and one was not provided.';
 
+module.exports.DB_DOC_NOT_FOUND = function(id) {
+    return `The requested document '${id}' does not exist in the database`;
+}
+
 module.exports.LIMIT_FILE_SIZE_ERROR = function(maximumSize) {
     return `The uploaded file exceeds the maximum file size supported of ${maximumSize/1024/1024}MB.`;
 }
