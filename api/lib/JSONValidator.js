0fda6d66525339bbb02527eacbf5403816704b4d
Gabriel R. Sezefredo <gabriel@sezefredo.com.br>
2018-06-10 15:20:33 -0300

    Closes #24 - Implements new "sanitize" method in JSONValidator
    This commit adds a "sanitize" function in our JSONValidator. this method receives a json document and a schema name and returns a json document containing only fields defined in the schema.
    
    A helper function "sanitizeList" is also implemented that receives a list and a schema name, and runs "sanitize" for each item of the list, passing the schema name.
    
    Signed-off-by: Gabriel R. Sezefredo <gabriel@sezefredo.com.br>

--- api/lib/JSONValidator.js
+++ api/lib/JSONValidator.js
@@ -20,4 +20,21 @@ JSONValidator.prototype.getSchema = function (schemaName) {
     return JSON.stringify(Schemas[schemaName], null, 2);
 }
 
+JSONValidator.prototype.sanitize = function(doc, schemaName) {
+    if (!this.validSchemas.includes(schemaName))
+        throw new Error(errorMessages.INVALID_SCHEMA_ERROR(this.validSchemas.join(', ')));
+    let validate = ajv.compile(Schemas[schemaName])
+    if (validate(doc)) return doc // Returns sanitized document
+    throw new Error(errorMessages.INVALID_SCHEMA_FIELDS_ERROR(schemaName, this.getSchema(schemaName)));
+}
+
+JSONValidator.prototype.sanitizeList = function (list, schemaName) {
+    if (!Array.isArray(list))
+        throw new Error(errorMessages.TYPE_MISMATCH_ERROR('list', 'array', typeof list))
+    list.forEach((el, i) => {
+        list[i] = this.sanitize(el, schemaName);
+    })
+    return list;
+}
+
 module.exports = JSONValidator;
