1f4871246f5140633b5bbd6ce2bbee85a3c9182a
Gabriel R. Sezefredo <gabriel@sezefredo.com.br>
2018-06-10 20:19:07 -0300

    Implements the POST /menus/:id/ocr (Add new OCR)
    This commit implements the business logic to add a new OCR text result into the OCR array of a given menu.
    
    Signed-off-by: Gabriel R. Sezefredo <gabriel@sezefredo.com.br>

--- api/v1/menus/ocr/index.js
+++ api/v1/menus/ocr/index.js
@@ -1,4 +1,6 @@
-const ocr = require('express').Router();
+const ocr = require('express').Router({mergeParams: true});
+const bodyParser = require('body-parser');
+const textParser = bodyParser.text();
 
 const getLast = require('./getLast');
 const getOne = require('./getOne');
@@ -8,7 +10,7 @@ const create = require('./create');
 // If query string is present, we redirect it to another end-point
 ocr.get('/', getLast);
 ocr.get('/size', size);
-ocr.get('/:id', getOne);
-ocr.post('/', create);
+ocr.get('/:index', getOne);
+ocr.post('/', textParser, create);
 
 module.exports = ocr;
