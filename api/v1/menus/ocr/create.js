058db5fa91596712dd7758f90dfdfc1b8ae63c8f
Gabriel R. Sezefredo <gabriel@sezefredo.com.br>
2018-06-10 20:19:07 -0300

    Implements the POST /menus/:id/ocr (Add new OCR)
    This commit implements the business logic to add a new OCR text result into the OCR array of a given menu.
    
    Signed-off-by: Gabriel R. Sezefredo <gabriel@sezefredo.com.br>

--- api/v1/menus/ocr/create.js
+++ api/v1/menus/ocr/create.js
@@ -1,4 +1,32 @@
-module.exports = (req, res) => {
-    // TODO: create OCR method
-    return res.send('TODO: create OCR method.');
+const DBClient = require('../../../lib/DBClient');
+
+const httpStatus = require('../../../lib/http-status-codes');
+const errorMessages = require('../../../lib/error-messages');
+
+
+module.exports = (req, res, next) => {
+
+    let docId = req.params.id
+    let ocr = req.body
+
+    // Uploading to box and saving to db
+    let dbInstance = new DBClient('menus');
+
+    dbInstance.getById(docId)
+        .then((dbEntry) => {
+            if (!dbEntry.ocr) dbEntry.ocr = [];
+            dbEntry.ocr.push(ocr)
+            return dbEntry
+        })
+        .then((dbEntry) => dbInstance.update(dbEntry))
+        .then(dbEntry => res.status(httpStatus.OK_CREATED).send(dbEntry))
+        .catch((err) => {
+            if (err.message === 'missing') {
+                err = new Error(errorMessages.DB_DOC_NOT_FOUND(req.params.id));
+                err.status = httpStatus.NOT_FOUND;
+            } else {
+                err = new Error(errorMessages.INTERNAL_SERVER_ERROR());
+            }
+            return next(err)
+        });
 };
