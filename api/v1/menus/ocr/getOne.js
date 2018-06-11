842b1ed1128c2f3b4e8f10824fe07be72c9f0626
Gabriel R. Sezefredo <gabriel@sezefredo.com.br>
2018-06-10 21:05:39 -0300

    Implements the GET /menus/:id/ocr/:index (Get ocr by number)
    This commit implements the route to get the OCR at the route param defined index.
    
    Signed-off-by: Gabriel R. Sezefredo <gabriel@sezefredo.com.br>

--- api/v1/menus/ocr/getOne.js
+++ api/v1/menus/ocr/getOne.js
@@ -1,4 +1,34 @@
-module.exports = (req, res) => {
-    // TODO: getOne OCR method.
-    return res.send('TODO: getOne OCR method.');
+const DBClient = require('../../../lib/DBClient');
+
+const httpStatus = require('../../../lib/http-status-codes');
+const errorMessages = require('../../../lib/error-messages');
+
+
+module.exports = (req, res, next) => {
+
+    let docId = req.params.id;
+    let index = req.params.index;
+
+    // Uploading to box and saving to db
+    let dbInstance = new DBClient('menus');
+
+    dbInstance.getById(docId)
+        .then((dbEntry) => {
+            const ocrLen = dbEntry.ocr ? dbEntry.ocr.length : 0;
+            if (ocrLen <= index) {
+                const err = new Error(errorMessages.OCR_INDEX_OUT_OF_BOUNDS_ERROR(ocrLen));
+                err.status = httpStatus.NOT_FOUND
+                return next(err);
+            }
+            return res.status(httpStatus.OK).send(dbEntry.ocr[index]);
+        })
+        .catch((err) => {
+            if (err.message === 'missing') {
+                err = new Error(errorMessages.DB_DOC_NOT_FOUND(req.params.id));
+                err.status = httpStatus.NOT_FOUND;
+            } else {
+                err = new Error(errorMessages.INTERNAL_SERVER_ERROR());
+            }
+            return next(err)
+        });
 };
