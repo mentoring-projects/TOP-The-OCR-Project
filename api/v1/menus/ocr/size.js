caaf219a61807a53f1592ecab9de006d92be2695
Gabriel R. Sezefredo <gabriel@sezefredo.com.br>
2018-06-10 21:15:37 -0300

    Implements the GET /menus/:id/ocr/size (Get ocr by number)
    This commit implements the route to get the OCR array length.
    
    Signed-off-by: Gabriel R. Sezefredo <gabriel@sezefredo.com.br>

--- api/v1/menus/ocr/size.js
+++ api/v1/menus/ocr/size.js
@@ -1,4 +1,28 @@
-module.exports = (req, res) => {
-    // TODO: size OCR method.
-    return res.send('TODO: size OCR method.');
+const DBClient = require('../../../lib/DBClient');
+
+const httpStatus = require('../../../lib/http-status-codes');
+const errorMessages = require('../../../lib/error-messages');
+
+
+module.exports = (req, res, next) => {
+
+    let docId = req.params.id;
+
+    // Uploading to box and saving to db
+    let dbInstance = new DBClient('menus');
+
+    dbInstance.getById(docId)
+        .then((dbEntry) => {
+            const ocrLen = dbEntry.ocr ? dbEntry.ocr.length : 0;
+            return res.status(httpStatus.OK).send(ocrLen.toString());
+        })
+        .catch((err) => {
+            if (err.message === 'missing') {
+                err = new Error(errorMessages.DB_DOC_NOT_FOUND(req.params.id));
+                err.status = httpStatus.NOT_FOUND;
+            } else {
+                err = new Error(errorMessages.INTERNAL_SERVER_ERROR());
+            }
+            return next(err)
+        });
 };
