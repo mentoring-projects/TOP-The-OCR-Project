fdedb1b0869f660602ef7b4f0c71d8a6edb12b3a
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2018-06-14 00:03:57 -0300

    Implement delete menu route
    This route receives an menu id as it parameter.
    There is no need to make an additional validation of this parameter
    because it comes already as 'string' and with length more than 0.
    
    It uses the getById function from the DBClient.js to retrive the
    actual _rev and fileId of this document. If it catchs an error of
    'missing' or 'deleted' means that the document wasn't found, so it
    falls in our lib error for DB_DOC_NOT_FOUND. Any error that don't
    qualify on the previous errors are considered INTERNAL_SERVER_ERROR.
    
    With the document deleted on the database, it's start the fsClient
    function to delete the file asynchronously with the success response
    to the user.
    
    Signed-off-by: Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>

--- api/v1/menus/delete.js
+++ api/v1/menus/delete.js
@@ -1,4 +1,40 @@
-module.exports = (req, res) => {
-    // TODO: delete menu method.
-    return res.send('TODO: delete menu method.');
+const FSClient = require('../../lib/FSClient');
+const DBClient = require('../../lib/DBClient');
+
+const httpStatus = require('../../lib/http-status-codes');
+const errorMessages = require('../../lib/error-messages');
+
+module.exports = (req, res, next) => {
+    let fsClient = new FSClient();
+    let dbClient = new DBClient('menus');
+    // getById is used to get the _rev and the fileId
+    dbClient.getById(req.params.id)
+        .then((resp) => {
+            let fileId = resp.file;
+            // delete uses the id and rev to delete the doc
+            dbClient.delete(resp._id, resp._rev).then((resp) => {
+                // while is processing the delete of the file in the 'box', the user receives a success message already.
+                fsClient.delete(fileId).then((data) => {
+                    console.log('File ' + fileId + ' from doc ' +
+                        req.params.id + ' was deleted successfully');
+                })
+                    .catch((err) => {
+                        console.log(err.stack)
+                    })
+                return res.status(httpStatus.OK).json(resp);
+            })
+                .catch((err) => {
+                    err = new Error(errorMessages.INTERNAL_SERVER_ERROR());
+                    return next(err)
+                })
+        })
+        .catch((err) => {
+            if (err.message === 'missing' || err.message === 'deleted') {
+                err = new Error(errorMessages.DB_DOC_NOT_FOUND(req.params.id));
+                err.status = httpStatus.NOT_FOUND;
+            } else {
+                err = new Error(errorMessages.INTERNAL_SERVER_ERROR());
+            }
+            return next(err)
+        })
 };
