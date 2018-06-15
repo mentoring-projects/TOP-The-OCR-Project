7f36fc83824e1d808346a10029c60d1547f7683f
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2018-06-15 12:21:44 -0300

    Add paging to get menus route
    This route is available with a get request to /api/v1/menus?from=?&size=?
    It's make a verification with the query params passed in the request.
    The params object receives the req.query.from and the req.query.size
    if they are defined, they are converted to Number to verify after if
    they are integer, otherwise the route throws an error.
    
    After pass to the route check the params are passed to the DbClient get
    function that has it's own verification of params too.
    
    Signed-off-by: Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>

--- api/v1/menus/slice.js
+++ api/v1/menus/slice.js
@@ -1,4 +1,26 @@
-module.exports = (req, res) => {
-    // TODO: slice menu method.
-    return res.send('TODO: slice menu method.');
+const DBClient = require('../../lib/DBClient');
+const httpStatus = require('../../lib/http-status-codes');
+const errorMessages = require('../../lib/error-messages');
+
+const dbInstance = new DBClient('menus');
+
+module.exports = (req, res, next) => {
+    let params = {
+        from: req.query.from,
+        size: req.query.size
+    }
+    for (let k in params) {
+        if (!params[k] || !Number.isInteger(Number(params[k]))) {
+            let err = new Error(errorMessages.TYPE_MISMATCH_ERROR(k, 'integer', typeof params[k]));
+            err.status = httpStatus.BAD_REQUEST;
+            return next(err)
+        }
+    }
+    dbInstance.get({ from: params.from, size: params.size })
+        .then((resp) => res.status(httpStatus.OK).json(resp))
+        .catch((error) => {
+            console.log(error.stack);
+            let err = new Error(errorMessages.DB_CONNECT_ERROR());
+            return next(err)
+        })
 };
