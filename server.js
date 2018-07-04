731bfb7bf8a65ed3e09ae71e1f0fbf3409ae2568
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2018-07-04 07:09:14 -0300

    Add OCR lib
    This lib takes care off the most important functionality of our
    App, that is to convert PDF scans to text.
    
    When you initiate a constructor you can specifie the language, psm
    and where your tesseract software is installed in your operating
    system. By default, the language is set to portuguese.
    
    To use the OCR in your PDF file, you must use the getText function
    that uses a private function called PDFtoImage to convert all your
    PDF pages to images and then run tesseract to convert the images in
    a readable string by the ImagesToText private function.
    
    Signed-off-by: Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>

--- server.js
+++ server.js
@@ -2,47 +2,15 @@ require('dotenv').config();
 const express = require('express'),
     api = require('./api'),
     bodyParser = require('body-parser'),
-    morgan = require('morgan'),
-    multer = require('multer');
-
-
+    morgan = require('morgan');
+    
 const app = express();
 app.use(express.static("public"));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-const upload = multer({ dest: "uploads/" }),
-    OCR = require('./ocr');
-
-
 // Configuring API routes
 app.use('/api', api);
-
-// Leaving it here for now, but we will probably move that to "/api/v1/menu/ocr"
-app.post('/v1/convert/ocr', upload.single('file_img'), (req, res) => {
-    var ocrInstance;
-    if (req.file) {
-        if (req.file.size < 28116350){
-            if (req.file.mimetype.search("image") !== -1){
-                ocrInstance = new OCR();
-                ocrInstance.getText(req.file.path, function(err, response){
-                    if (err){
-                        res.status(500).json({ERROR : "Error to convert the image!"});
-                    } else {
-                        res.status(200).send(response);
-                    }
-                });
-            } else {
-                res.status(400).json({ERROR : "Attachment must be png, jpeg or jpg!"});
-            }
-        } else {
-            res.status(400).json({ERROR : "Attachment too long!"});
-        }
-    } else {
-        res.status(400).json({ERROR : "Empty attachment!"});
-    }
-});
-
 
 app.listen(process.env.PORT, () => console.log(`Server listening at port ${process.env.PORT}`));
